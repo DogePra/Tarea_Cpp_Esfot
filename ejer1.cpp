ad4db26ddf8296a4989ccfb42d53a4a48107054e
DogePra <andramuniomateo@gmail.com>
2025-11-30 22:06:52 -0500

    Add files via upload

---
+++ ejer1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string ciudades[5];
+
+    ciudades[0] = "Quito";
+    ciudades[1] = "Guayaquil";
+    ciudades[2] = "Cuenca";
+    ciudades[3] = "Ambato";
+    ciudades[4] = "Manta";
+
+    cout << "La ciudad del tercer registro es: " << ciudades[2] << endl;
+
+    return 0;
+}
